dadeb8a099a6b0416241438b340a91937b883505
Doriabn <86948293+Doriabn@users.noreply.github.com>
2024-10-02 11:30:26 -0500

    Delete demoA directory

--- demoA/src/main/java/pe/edu/upc/demoa/dtos/ReportesDTO.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.upc.demoa.dtos;
-
-import lombok.Data;
-
-@Data
-public class ReportesDTO {
-    private String nombreAutor;
-    private int cantidadLibros;
-}
--- demoA/src/main/java/pe/edu/upc/demoa/repository/UsuarioRepository.java
+++
@@ -1,8 +0,0 @@
-package pe.edu.upc.demoa.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pe.edu.upc.demoa.entities.Usuario;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
-    public Usuario findOneByUsername (String nombreUsuario);
-}
